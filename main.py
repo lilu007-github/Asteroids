7cf73f6a6b8b88bd6eacc45b5901ae256473255b
lilu007-github <lilu007_snow@hotmail.com>
2025-10-20 22:02:15 -0700

    add shot related change

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -21,8 +23,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
 
     Asteroid.containers = (updatable, drawable, asteroids)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers =  updatable
     asteroid_field = AsteroidField()
 
@@ -40,8 +45,12 @@ def main():
                 return
         
         updatable.update(dt)  # Update all updatable sprites
-        
-    
+
+        for asteroid in asteroids:
+            if asteroid.collide_with(player):
+                print("Game Over!")
+                sys.exit()
+
         screen.fill((0, 0, 0))  # Fill the screen with black
 
         for obj in drawable:
